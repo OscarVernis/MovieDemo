e5a253aed0eeb578f6f417c9a5db960c3da8746d
Oscar Vernis <oscar.vernis@gmail.com>
2022-03-25 12:50:05 -0600

    Refactoring MovieService tests.

--- MovieDemo/MovieDemoTests/MovieServiceTests.swift
+++ MovieDemo/MovieDemoTests/MovieServiceTests.swift
@@ -7,26 +7,19 @@
 //
 
 import XCTest
-import Combine
 @testable import MovieDemo
 
 class MovieServiceTests: XCTestCase {
-    private var cancellables: Set<AnyCancellable>!
-    
-    override func setUpWithError() throws {
-        cancellables = []
-    }
     
     func test_getModel_success() {
         let movieId = 155
         let url = URL(string: "https://api.themoviedb.org/3/movie/\(movieId)")!
-        let mocker = MockData()
+        let mocker = MockData(jsonFile: "Movie", url: url)
         
-        mocker.register(jsonFile: "Movie", url: url)
-        
-        var movie: Movie?
         let sut = MovieService(session: mocker.session)
-        movie = try! awaitPublisher( sut.getModel(endpoint: .MovieDetails(movieId: movieId)) )
+        var movie: Movie?
+        
+        XCTAssertNoThrow(movie = try awaitPublisher( sut.getModel(endpoint: .MovieDetails(movieId: movieId)) ))
         
         XCTAssertEqual(movie?.id, movieId)
         XCTAssertEqual(movie?.title, "The Dark Knight")
@@ -34,10 +27,12 @@ class MovieServiceTests: XCTestCase {
     
     func test_getModels_success() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
-        let mocker = MockData()
+        let mocker = MockData(jsonFile: "Movies", url: url)
         
-        mocker.register(jsonFile: "Movies", url: url)
-        let results: (movies: [Movie], totalPages: Int)? = runGetModels(mocker: mocker, endpoint: .NowPlaying)
+        let sut = MovieService(session: mocker.session)
+        var results: (movies: [Movie], totalPages: Int)?
+        
+        XCTAssertNoThrow(results = try awaitPublisher( sut.getModels(endpoint: .NowPlaying) ))
         
         XCTAssertEqual(results?.movies.count, 20)
         XCTAssertEqual(results?.totalPages, 33)
@@ -45,46 +40,36 @@ class MovieServiceTests: XCTestCase {
     
     func test_getModels_failsOnEmptyResult() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
-        let mocker = MockData()
-        
         let emptyResult = [String:String]()
-        mocker.register(jsonObject: emptyResult, url: url)
-
-        let results: ([Movie], Int)? = runGetModels(mocker: mocker, endpoint: .NowPlaying)
+        let mocker = MockData(jsonObject: emptyResult, url: url)
         
+        let sut = MovieService(session: mocker.session)
+        var results: (movies: [Movie], totalPages: Int)?
+        
+        XCTAssertThrowsError(results = try awaitPublisher( sut.getModels(endpoint: .NowPlaying) ))
         XCTAssertNil(results)
     }
     
     func test_getModels_failsOn401Status() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
-        let mocker = MockData()
+        let mocker = MockData(jsonFile: "Movies", url: url, statusCode: 401)
         
-        mocker.register(jsonFile: "Movies", url: url, statusCode: 401)
-        let results: ([Movie], Int)? = runGetModels(mocker: mocker, endpoint: .NowPlaying)
+        let sut = MovieService(session: mocker.session)
+        var results: (movies: [Movie], totalPages: Int)?
         
+        XCTAssertThrowsError(results = try awaitPublisher( sut.getModels(endpoint: .NowPlaying) ))
         XCTAssertNil(results)
     }
     
     func test_getModels_failsOn404Status() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing")!
-        let mocker = MockData()
+        let mocker = MockData(jsonFile: "Movies", url: url, statusCode: 404)
         
-        mocker.register(jsonFile: "Movies", url: url, statusCode: 404)
-        let results: ([Movie], Int)? = runGetModels(mocker: mocker, endpoint: .NowPlaying)
+        let sut = MovieService(session: mocker.session)
+        var results: (movies: [Movie], totalPages: Int)?
         
+        XCTAssertThrowsError(results = try awaitPublisher( sut.getModels(endpoint: .NowPlaying) ))
         XCTAssertNil(results)
     }
 
-    //MARK: - Helpers
-    func runGetModels<T>(mocker: MockData, endpoint: MovieService.Endpoint) -> T? {
-        var results: T? = nil
-        let sut = MovieService(session: mocker.session)
-        do {
-            results = try awaitPublisher( sut.getModels(model: Movie.self, endpoint: endpoint) ) as? T
-        } catch(let error) {
-            XCTAssertNotNil(error)
-        }
-        
-        return results
-    }
 }
