5e29a2dbe18e97b61cf0d283ae560c200719b4b9
Oscar Vernis <oscar.vernis@gmail.com>
2022-05-24 13:15:24 -0500

    Added Async extension for Combine publishers.

---
+++ MovieDemo/MovieDemo/Extensions/AnyPublisher+Async.swift
@@ -0,0 +1,31 @@
+//
+//  AnyPublisher+Async.swift
+//  MovieDemo
+//
+//  Created by Oscar Vernis on 24/05/22.
+//  Copyright © 2022 Oscar Vernis. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+extension AnyPublisher {
+    func async() async throws -> Output {
+        try await withCheckedThrowingContinuation { continuation in
+            var cancellable: AnyCancellable?
+            
+            cancellable = first()
+                .sink { result in
+                    switch result {
+                    case .finished:
+                        break
+                    case let .failure(error):
+                        continuation.resume(throwing: error)
+                    }
+                    cancellable?.cancel()
+                } receiveValue: { value in
+                    continuation.resume(with: .success(value))
+                }
+        }
+    }
+}
--- MovieDemo/MovieDemo/Remote/MovieService.swift
+++ MovieDemo/MovieDemo/Remote/MovieService.swift
@@ -145,35 +145,6 @@ extension MovieService {
     
 }
 
-//MARK: - async/await
-extension MovieService {
-    func successAction<T: Encodable>(endpoint: Endpoint, body: T?, method: HTTPMethod = .get) async throws -> ServiceSuccessResult {
-        try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-                        
-            let publisher = successAction(endpoint: endpoint, body: body, method: method)
-            
-            cancellable = publisher
-                .sink { result in
-                    switch result {
-                    case .finished:
-                        break
-                    case let .failure(error):
-                        continuation.resume(throwing: error)
-                    }
-                    cancellable?.cancel()
-                } receiveValue: { value in
-                    continuation.resume(with: .success(value) )
-                }
-        }
-    }
-    
-    func successAction(endpoint: Endpoint, method: HTTPMethod = .get) async throws -> ServiceSuccessResult {
-        return try await successAction(endpoint: endpoint, body: Optional<String>.none, method: method)
-    }
-    
-}
-
 //MARK: - Image Utils
 extension MovieService {
     enum MoviePosterSize: String {
