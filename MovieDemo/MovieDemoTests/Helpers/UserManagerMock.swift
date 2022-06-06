6abd036d93e49c1e7a3de0e25bb93096bc1f3941
Oscar Vernis <oscar.vernis@gmail.com>
2022-06-06 15:24:16 -0500

    Fixed UserManagerMock.

--- MovieDemo/MovieDemoTests/Helpers/UserManagerMock.swift
+++ MovieDemo/MovieDemoTests/Helpers/UserManagerMock.swift
@@ -11,24 +11,20 @@ import Foundation
 
 class UserStoreMock: UserStore {
     var sessionId: String?
-    var username: String?
     
-    init(sessionId: String? = nil, username: String? = nil, isLoggedIn: Bool = false) {
+    init(sessionId: String? = nil, isLoggedIn: Bool = false) {
         self.sessionId = sessionId
-        self.username = username
         
         if isLoggedIn && self.sessionId == nil {
             self.sessionId = "sessionId"
         }
     }
     
-    func save(username: String, sessionId: String) {
+    func save(sessionId: String) {
         self.sessionId = sessionId
-        self.username = username
     }
     
     func delete() {
         self.sessionId = nil
-        self.username = nil
     }
 }
